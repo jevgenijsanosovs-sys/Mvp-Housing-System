03f8d4bbbecb5ab1ac2fc1ed594246bf066291a4
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-06-14 12:52:59 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
 import {
   RouterProvider,
 } from "react-router-dom";
+
 import { router } from "./router/router";
+
 import {
   AuthProvider,
 } from "./context/AuthContext";
 
+import {
+  ModeProvider,
+} from "./context/ModeContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(
+  document.getElementById("root")
+).render(
+
   <StrictMode>
-	  <AuthProvider>
-		  <RouterProvider
-			router={router}
-		  />
-	  </AuthProvider>
-  </StrictMode>,
-)
+
+    <AuthProvider>
+
+      <ModeProvider>
+
+        <RouterProvider
+          router={router}
+        />
+
+      </ModeProvider>
+
+    </AuthProvider>
+
+  </StrictMode>
+
+);
