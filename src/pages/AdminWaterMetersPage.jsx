c0fca182cdb9c96fe3396d40f73e0d94bcc05aee
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-06-26 08:59:17 +0300

    Add AdminWaterMetersPage component

---
+++ src/pages/AdminWaterMetersPage.jsx
@@ -0,0 +1,19 @@
+export default function AdminWaterMetersPage() {
+
+  return (
+
+    <div>
+
+      <h1>
+        Water Meter Management
+      </h1>
+
+      <p>
+        Module under development.
+      </p>
+
+    </div>
+
+  );
+
+}
