f0072d730932afee102e7cd4994b22aaff978afd
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-06-29 13:47:28 +0300

    Add WaterMetersPage component with placeholder content

---
+++ src/pages/WaterMetersPage.jsx
@@ -0,0 +1,19 @@
+export default function WaterMetersPage() {
+
+  return (
+
+    <div>
+
+      <h1>
+        Water Meter Management
+      </h1>
+
+      <p>
+        Coming soon...
+      </p>
+
+    </div>
+
+  );
+
+}
