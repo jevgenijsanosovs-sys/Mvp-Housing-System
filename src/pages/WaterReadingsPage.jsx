8e1fa5c716cdea5c9c2c4f1d1d568dc8ebae3d1f
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-06-29 13:41:33 +0300

    Create WaterReadingsPage.jsx

---
+++ src/pages/WaterReadingsPage.jsx
@@ -0,0 +1,68 @@
+import { useEffect } from "react";
+
+import useWater from "../hooks/useWater";
+
+import {
+  tableStyle,
+} from "../styles/theme";
+
+export default function AdminWaterPage() {
+
+  const {
+    adminWater,
+    loadAdminWater,
+  } = useWater();
+
+  useEffect(() => {
+    loadAdminWater();
+  }, []);
+
+  return (
+    <div>
+
+      <h1>
+        Water Readings
+      </h1>
+
+      <table style={tableStyle}>
+
+        <thead>
+          <tr>
+            <th>Apartment</th>
+            <th>Type</th>
+            <th>Serial</th>
+            <th>Value</th>
+            <th>Date</th>
+            <th>User</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          {adminWater.map((r, i) => (
+
+            <tr key={i}>
+
+              <td>{r.apartment_number}</td>
+              <td>{r.type}</td>
+              <td>{r.serial_number}</td>
+              <td>{r.reading_value}</td>
+              <td>{r.reading_date}</td>
+
+              <td>
+                {r.first_name}
+                {" "}
+                {r.last_name}
+              </td>
+
+            </tr>
+
+          ))}
+
+        </tbody>
+
+      </table>
+
+    </div>
+  );
+}
