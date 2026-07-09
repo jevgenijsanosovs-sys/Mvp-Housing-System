28fc23f8066fd4d57f3a738d92ac016bcf6c4c30
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-09 13:23:25 +0300

    Update ApartmentWaterTab.jsx

--- src/components/apartment/ApartmentWaterTab.jsx
+++ src/components/apartment/ApartmentWaterTab.jsx
@@ -6,44 +6,63 @@ export default function ApartmentWaterTab({
 
     <div>
 
-      {apartment.risers?.map(
-        (riser) => (
+      {apartment.risers?.map((riser) => (
 
-          <div
-            key={riser.name}
+        <div
+          key={riser.name}
+          style={{
+            marginBottom: 24,
+          }}
+        >
+
+          <h3
             style={{
-              marginBottom: 20,
+              marginBottom: 12,
+              color: "#334155",
             }}
           >
+            {riser.name}
+          </h3>
 
-            <h4>
-              {riser.name}
-            </h4>
+          {riser.meters.map((meter) => (
 
-            {riser.meters.map(
-              (meter) => (
+            <div
+              key={meter.id}
+              style={{
+                border: "1px solid #e5e7eb",
+                borderRadius: 12,
+                padding: 14,
+                marginBottom: 10,
+                background: "#fff",
+              }}
+            >
 
-                <div
-                  key={meter.id}
-                >
+              <div
+                style={{
+                  fontWeight: 600,
+                }}
+              >
+                {meter.type === "hot"
+                  ? "🔴 Hot Water"
+                  : "🔵 Cold Water"}
+              </div>
 
-                  {meter.type === "hot"
-                    ? "🔴"
-                    : "🔵"}
+              <div
+                style={{
+                  color: "#64748b",
+                  fontSize: 14,
+                }}
+              >
+                SN {meter.serial_number}
+              </div>
 
-                  {" "}
+            </div>
 
-                  {meter.serial_number}
+          ))}
 
-                </div>
+        </div>
 
-              )
-            )}
-
-          </div>
-
-        )
-      )}
+      ))}
 
     </div>
 
