36ac1ecb9ef4559124ffcd8546c44bc5a2437284
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-11 14:44:41 +0300

    Refactor ApartmentDetails component structure

--- src/components/ApartmentDetails.jsx
+++ src/components/ApartmentDetails.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 
 import TabBar from "./TabBar";
-
 import SectionCard from "./SectionCard";
+
 import ApartmentWaterTab
   from "./apartment/ApartmentWaterTab";
 
 export default function ApartmentDetails({
-
   apartment,
-
 }) {
 
   const [tab, setTab] = useState("Water");
@@ -18,73 +16,62 @@ export default function ApartmentDetails({
     selectedMeter,
     setSelectedMeter,
   ] = useState(null);
-  
 
   if (!apartment) {
-
     return null;
-
   }
 
   return (
 
     <div>
 
-
       <TabBar
-
         tabs={[
-
           "Water",
-
           "Residents",
-
           "Documents",
-
           "Tasks",
-
           "History",
-
         ]}
-
         value={tab}
-
         onChange={setTab}
-
       />
 
       {tab === "Water" && (
-      
-        <ApartmentWaterTab
-          apartment={apartment}
-          onOpenMeter={setSelectedMeter}
-        />
 
-        {selectedMeter && (
-        
-          <div
-            style={{
-              marginBottom: 20,
-              padding: 12,
-              borderRadius: 10,
-              background: "#dcfce7",
-              color: "#166534",
-              fontWeight: 600,
-            }}
-          >
-            Selected meter:{" "}
-            {selectedMeter.serial_number}
-          </div>
-        
-        )}
+        <>
 
-      
+          <ApartmentWaterTab
+            apartment={apartment}
+            onOpenMeter={setSelectedMeter}
+          />
+
+          {selectedMeter && (
+
+            <div
+              style={{
+                marginBottom: 20,
+                padding: 12,
+                borderRadius: 10,
+                background: "#dcfce7",
+                color: "#166534",
+                fontWeight: 600,
+              }}
+            >
+              Selected meter:{" "}
+              {selectedMeter.serial_number}
+            </div>
+
+          )}
+
+        </>
+
       )}
 
       {tab === "Residents" && (
-      
+
         <SectionCard>
-      
+
           <div
             style={{
               padding: 40,
@@ -94,15 +81,15 @@ export default function ApartmentDetails({
           >
             No residents data
           </div>
-      
+
         </SectionCard>
-      
+
       )}
-      
+
       {tab === "Documents" && (
-      
+
         <SectionCard>
-      
+
           <div
             style={{
               padding: 40,
@@ -112,15 +99,15 @@ export default function ApartmentDetails({
           >
             No documents
           </div>
-      
+
         </SectionCard>
-      
+
       )}
-      
+
       {tab === "Tasks" && (
-      
+
         <SectionCard>
-      
+
           <div
             style={{
               padding: 40,
@@ -130,28 +117,28 @@ export default function ApartmentDetails({
           >
             No tasks
           </div>
-      
+
         </SectionCard>
-      
+
       )}
 
-{tab === "History" && (
+      {tab === "History" && (
 
-  <SectionCard>
+        <SectionCard>
 
-    <div
-      style={{
-        padding: 40,
-        textAlign: "center",
-        color: "#64748b",
-      }}
-    >
-      No history
-    </div>
+          <div
+            style={{
+              padding: 40,
+              textAlign: "center",
+              color: "#64748b",
+            }}
+          >
+            No history
+          </div>
 
-  </SectionCard>
+        </SectionCard>
 
-)}
+      )}
 
     </div>
 
