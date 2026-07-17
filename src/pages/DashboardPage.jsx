94b95c727e9c61c99a2d8086091ae2cd679cd938
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-17 13:04:07 +0300

    Increase font size from 12 to 13 in DashboardPage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -278,7 +278,7 @@ export default function DashboardPage() {
           <h1
             style={{
               margin: 0,
-              fontSize: 12,
+              fontSize: 13,
               alignItems: "left",
             }}
           >
