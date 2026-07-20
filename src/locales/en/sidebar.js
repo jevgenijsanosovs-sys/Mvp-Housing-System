d9cb94d56b4f20542218eff95da9ef2d2b664e7c
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 13:38:03 +0300

    Add sidebar localization for English

---
+++ src/locales/en/sidebar.js
@@ -0,0 +1,18 @@
+const sidebar = {
+  user: "User",
+  residentMode: "Resident Mode",
+  adminMode: "Admin Mode",
+  dashboard: "Dashboard",
+  waterMeters: "Water Meters",
+  announcements: "Announcements",
+  users: "Users",
+  apartments: "Apartments",
+  waterMeterManagement:
+    "Water Meter Management",
+  waterReadingHistory:
+    "Water Reading History",
+  monthlyReport: "Monthly Report",
+  logout: "Logout",
+};
+
+export default sidebar;
