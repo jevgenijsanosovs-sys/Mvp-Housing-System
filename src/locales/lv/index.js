9f47b7089499e253bd9f1abc6e52009ea61551fe
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 14:58:03 +0300

    Add water localization to Latvian module

--- src/locales/lv/index.js
+++ src/locales/lv/index.js
@@ -1,11 +1,13 @@
 import common from "./common";
 import dashboard from "./dashboard";
 import sidebar from "./sidebar";
+import water from "./water";
 
 const lv = {
   common,
   dashboard,
   sidebar,
+  water,
 };
 
 export default lv;
