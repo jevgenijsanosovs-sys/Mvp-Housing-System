c9b89af3ef8111ef27201aa9e8b8d10d5839c4bb
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 10:27:57 +0300

    Add admin announcement management functions

--- src/api/announcements.js
+++ src/api/announcements.js
@@ -17,3 +17,65 @@ export async function getAnnouncement(
     )}`
   );
 }
+
+export async function getAdminAnnouncements() {
+  return await api(
+    "/api/admin/announcements"
+  );
+}
+
+export async function createAnnouncement(
+  announcement
+) {
+  return await api(
+    "/api/admin/announcements",
+    {
+      method: "POST",
+      body: JSON.stringify(
+        announcement
+      ),
+    }
+  );
+}
+
+export async function updateAnnouncement(
+  announcement
+) {
+  return await api(
+    "/api/admin/update-announcement",
+    {
+      method: "POST",
+      body: JSON.stringify(
+        announcement
+      ),
+    }
+  );
+}
+
+export async function publishAnnouncement(
+  announcementId
+) {
+  return await api(
+    "/api/admin/publish-announcement",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        id: announcementId,
+      }),
+    }
+  );
+}
+
+export async function archiveAnnouncement(
+  announcementId
+) {
+  return await api(
+    "/api/admin/archive-announcement",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        id: announcementId,
+      }),
+    }
+  );
+}
