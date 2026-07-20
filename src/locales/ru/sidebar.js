06a80cd9f312200428540fc23318d334a04fc4ef
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 13:38:36 +0300

    Add Russian translations for sidebar labels

---
+++ src/locales/ru/sidebar.js
@@ -0,0 +1,18 @@
+const sidebar = {
+  user: "Пользователь",
+  residentMode: "Режим жильца",
+  adminMode: "Режим администратора",
+  dashboard: "Главная",
+  waterMeters: "Счётчики воды",
+  announcements: "Объявления",
+  users: "Пользователи",
+  apartments: "Квартиры",
+  waterMeterManagement:
+    "Управление счётчиками воды",
+  waterReadingHistory:
+    "История показаний воды",
+  monthlyReport: "Ежемесячный отчёт",
+  logout: "Выйти",
+};
+
+export default sidebar;
