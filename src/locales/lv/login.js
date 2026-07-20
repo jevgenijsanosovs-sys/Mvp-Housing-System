f0b151fe4da7db12ddfc36a016f897f62fe9f0a4
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 16:15:27 +0300

    Add Latvian localization for login page

---
+++ src/locales/lv/login.js
@@ -0,0 +1,15 @@
+const login = {
+  title: "MVX System",
+
+  email: "E-pasts",
+  password: "Parole",
+
+  login: "Pieslēgties",
+
+  placeholders: {
+    email: "E-pasts",
+    password: "Parole",
+  },
+};
+
+export default login;
