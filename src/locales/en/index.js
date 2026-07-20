0d2a3eeea0ff135c13d83ee508e737d5f81703e1
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 14:56:04 +0300

    Add water localization to English translations

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -1,11 +1,13 @@
 import common from "./common";
 import dashboard from "./dashboard";
 import sidebar from "./sidebar";
+import water from "./water";
 
 const en = {
   common,
   dashboard,
   sidebar,
+  water,
 };
 
 export default en;
