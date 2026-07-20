c00e51f27dd6300447848b734d419c74308b0d2f
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 13:36:56 +0300

    Create sidebar.js

---
+++ src/locales/lv/sidebar.js
@@ -0,0 +1,18 @@
+const sidebar = {
+  user: "Lietotājs",
+  residentMode: "Iedzīvotāja režīms",
+  adminMode: "Administratora režīms",
+  dashboard: "Informācijas panelis",
+  waterMeters: "Ūdens skaitītāji",
+  announcements: "Paziņojumi",
+  users: "Lietotāji",
+  apartments: "Dzīvokļi",
+  waterMeterManagement:
+    "Ūdens skaitītāju pārvaldība",
+  waterReadingHistory:
+    "Ūdens rādījumu vēsture",
+  monthlyReport: "Mēneša pārskats",
+  logout: "Iziet",
+};
+
+export default sidebar;
