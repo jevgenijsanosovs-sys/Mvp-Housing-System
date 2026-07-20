dbaa3b6063549765f197c2463331f9bf4e71f039
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 15:51:01 +0300

    Import announcements in Latvian locale module

--- src/locales/lv/index.js
+++ src/locales/lv/index.js
@@ -1,9 +1,11 @@
+import announcements from "./announcements";
 import common from "./common";
 import dashboard from "./dashboard";
 import sidebar from "./sidebar";
 import water from "./water";
 
 const lv = {
+  announcements,
   common,
   dashboard,
   sidebar,
