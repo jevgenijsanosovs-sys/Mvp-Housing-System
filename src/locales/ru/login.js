c532e6f1949187b0136a0758906d7ce959d0a068
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 16:16:23 +0300

    Add Russian localization for login page

---
+++ src/locales/ru/login.js
@@ -0,0 +1,15 @@
+const login = {
+  title: "MVX System",
+
+  email: "Электронная почта",
+  password: "Пароль",
+
+  login: "Войти",
+
+  placeholders: {
+    email: "Электронная почта",
+    password: "Пароль",
+  },
+};
+
+export default login;
