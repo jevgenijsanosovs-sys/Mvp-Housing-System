3e660047dcab4fc310e09c141912fce91f6a80ae
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 12:58:55 +0300

    Add index.js for i18n exports

---
+++ src/i18n/index.js
@@ -0,0 +1,11 @@
+export {
+  LanguageProvider,
+  SUPPORTED_LANGUAGES,
+  DEFAULT_LANGUAGE,
+  LANGUAGE_STORAGE_KEY,
+  normalizeLanguage,
+} from "./LanguageContext";
+
+export {
+  default as useTranslation,
+} from "./useTranslation";
