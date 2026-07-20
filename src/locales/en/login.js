2d433ff9363cdb0326b76d696f3ff4463923ebb2
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 16:15:57 +0300

    Add English login localization file

---
+++ src/locales/en/login.js
@@ -0,0 +1,15 @@
+const login = {
+  title: "MVX System",
+
+  email: "Email",
+  password: "Password",
+
+  login: "Login",
+
+  placeholders: {
+    email: "Email",
+    password: "Password",
+  },
+};
+
+export default login;
