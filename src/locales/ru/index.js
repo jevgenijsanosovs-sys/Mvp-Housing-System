25dbd7aaa4a0941c9345a5a9266e0293ac1b45c5
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 15:00:14 +0300

    Add water localization to Russian translations

--- src/locales/ru/index.js
+++ src/locales/ru/index.js
@@ -1,11 +1,13 @@
 import common from "./common";
 import dashboard from "./dashboard";
 import sidebar from "./sidebar";
+import water from "./water";
 
 const ru = {
   common,
   dashboard,
   sidebar,
+  water,
 };
 
 export default ru;
