80a03e6add51998cd8e6b8faed325779dcd46e31
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-20 15:52:17 +0300

    Import announcements in Russian locale

--- src/locales/ru/index.js
+++ src/locales/ru/index.js
@@ -1,9 +1,11 @@
+import announcements from "./announcements";
 import common from "./common";
 import dashboard from "./dashboard";
 import sidebar from "./sidebar";
 import water from "./water";
 
 const ru = {
+  announcements,
   common,
   dashboard,
   sidebar,
