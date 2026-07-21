98df058ded7bf93c679d05a484bbe7ed9d90c104
jevgenijsanosovs-sys <jevgenijs.anosovs@gmail.com>
2026-07-21 13:22:42 +0300

    Create router configuration for application pages
    This file sets up the router for the application, defining routes for various pages including login, dashboard, users, apartments, and more.

---
+++ src/router/index.jsx
@@ -0,0 +1,134 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+
+import App from "../App";
+
+import LoginPage
+  from "../pages/LoginPage";
+
+import DashboardPage
+  from "../pages/DashboardPage";
+
+import UsersPage
+  from "../pages/UsersPage";
+
+import ApartmentsPage
+  from "../pages/ApartmentsPage";
+
+import ResidentWaterPage
+  from "../pages/ResidentWaterPage";
+
+import WaterReadingsPage
+  from "../pages/WaterReadingsPage";
+
+import WaterMetersPage
+  from "../pages/WaterMetersPage";
+
+import AdminMonthlyReportPage
+  from "../pages/AdminMonthlyReportPage";
+
+import AnnouncementsPage
+  from "../pages/AnnouncementsPage";
+
+import AdminAnnouncementsPage
+  from "../pages/AdminAnnouncementsPage";
+
+import AnnouncementDetailsPage
+  from "../pages/AnnouncementDetailsPage";
+
+import SettingsPage
+  from "../pages/SettingsPage";
+
+import ProtectedRoute
+  from "../routes/ProtectedRoute";
+
+export const router =
+  createBrowserRouter([
+
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+
+    {
+      path: "/",
+
+      element: (
+        <ProtectedRoute>
+          <App />
+        </ProtectedRoute>
+      ),
+
+      children: [
+
+        {
+          index: true,
+          element:
+            <DashboardPage />,
+        },
+
+        {
+          path: "users",
+          element:
+            <UsersPage />,
+        },
+
+        {
+          path: "apartments",
+          element:
+            <ApartmentsPage />,
+        },
+
+        {
+          path: "water",
+          element:
+            <ResidentWaterPage />,
+        },
+
+        {
+          path: "announcements",
+          element:
+            <AnnouncementsPage />,
+        },
+
+        {
+          path: "announcement",
+          element:
+            <AnnouncementDetailsPage />,
+        },
+
+        {
+          path: "admin-announcements",
+          element:
+            <AdminAnnouncementsPage />,
+        },
+
+        {
+          path: "water-readings",
+          element:
+            <WaterReadingsPage />,
+        },
+
+        {
+          path: "water-meters",
+          element:
+            <WaterMetersPage />,
+        },
+
+        {
+          path: "monthly-report",
+          element:
+            <AdminMonthlyReportPage />,
+        },
+
+        {
+          path: "settings",
+          element:
+            <SettingsPage />,
+        },
+
+      ],
+    },
+
+  ]);
